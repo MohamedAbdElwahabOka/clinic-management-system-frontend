3a1515d32c7f3e7b7288d1a53315a602a33c341d
MohamedAbdElwahabOka <boka22986@gmail.com>
2025-02-27 02:56:41 +0200

    Add utility functions, update routing locales, and enhance Tailwind CSS configuration

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,9 @@
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
-import {notFound} from 'next/navigation';
-import {routing} from '@/i18n/routing';
-import LocalSwitcher from '@/components/local-switcher';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
+import Header from '@/components/header';
+import Sidebar from '@/components/Sidebar';
 import "./globals.css";
 
 export default async function LocaleLayout({
@@ -10,7 +11,7 @@ export default async function LocaleLayout({
   params
 }: {
   children: React.ReactNode;
-  params: Promise<{locale: string}>;
+  params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
 
@@ -23,66 +24,24 @@ export default async function LocaleLayout({
   // side is the easiest way to get started
   const messages = await getMessages();
 
-   // Determine the direction of the document
-   const dir = locale === 'ar' ? 'rtl' : 'ltr';
+  // Determine the direction of the document
+  const dir = locale === 'ar' ? 'rtl' : 'ltr';
 
-  console.log("messages   "+locale);
+  console.log("messages   " + locale);
 
   return (
     <html lang={locale} dir={dir}>
-      <body>
+      <body className="flex h-screen overflow-hidden">
         <NextIntlClientProvider messages={messages}>
-          <LocalSwitcher/>
-          {children}
+          <Sidebar />
+          <div className="flex flex-col flex-1">
+            <Header />
+            <main className="flex-1 overflow-auto">
+              {children}
+            </main>
+          </div>
         </NextIntlClientProvider>
       </body>
     </html>
   );
 }
-
-
-// import { NextIntlClientProvider } from 'next-intl';
-// import { getMessages } from 'next-intl/server';
-// import { notFound } from 'next/navigation';
-// import { routing } from '@/i18n/routing';
-// import LocalSwitcher from '@/components/local-switcher';
-// import "./globals.css";
-
-// export default async function LocaleLayout({
-//   children,
-//   params
-// }: {
-//   children: React.ReactNode;
-//   params: Promise<{ locale: string }>;
-// }) {
-//   const { locale } = await params;
-
-//   // Ensure that the incoming `locale` is valid
-//   if (!routing.locales.includes(locale as any)) {
-//     notFound();
-//   }
-
-//   // Providing all messages to the client
-//   // side is the easiest way to get started
-//   const messages = await getMessages();
-
-//   // Determine the direction of the document
-//   const dir = locale === 'ar' ? 'rtl' : 'ltr';
-//   const langClass = locale === 'ar' ? 'rtl' : 'ltr';
-
-//   console.log("messages   " + locale);
-
-//   return (
-//     <html lang={locale} dir={dir} className={langClass}>
-//       <body>
-//         <NextIntlClientProvider messages={messages}>
-//           <LocalSwitcher />
-//           {children}
-//         </NextIntlClientProvider>
-//       </body>
-//     </html>
-//   );
-// }
-
-
-
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,16 @@
 import {useTranslations} from 'next-intl';
 import {Link} from '@/i18n/routing';
 
+
  
 export default function HomePage() {
-  const t = useTranslations('HomePage');
+  // const t = useTranslations('HomePage');
   return (
  <div>
-    <h1 className='text-sky-500'>{t('title')}</h1>
-    <Link href="/about">{t('about')}</Link>
+
+    
+    {/* <h1 className='text-sky-500'>{t('title')}</h1>
+    <Link href="/about">{t('about')}</Link> */}
     
 </div>
     
